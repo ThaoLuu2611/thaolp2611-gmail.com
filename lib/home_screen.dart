1542c67cecd6bccb5325450817edae1a1341812a
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-03-26 10:53:48 +0700

    draw stroke

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,8 +9,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hello world')
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Custom paint'),
+      ),
+      body: Container(
+        color: Colors.amber,
+        padding: EdgeInsets.all(20),
+        child: LayoutBuilder(
+          builder: (_, constraints) => Container(
+          width: constraints.widthConstraints().maxWidth,
+          height: constraints.heightConstraints().maxHeight,
+          child: Container(
+            width: 300,
+            height: 500,
+            child: CustomPaint(painter: FaceSmilePainter() ),
+          ),
+        ))
+      ),
     );
   }
+
+}
+
+class FaceSmilePainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 4.0
+    ..color = Colors.indigo;
+      canvas.drawRect(
+          Rect.fromLTRB(20,20,100,100), paint);
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate)  => false;
+
 }
